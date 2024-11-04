c006b523ae22e733fae739f9507400006298969e
Krzyku <jasinski.it@gmail.com>
2024-11-04 14:46:13 +0100

    format files

--- template.ts
+++ template.ts
@@ -1,27 +1,27 @@
 import solve from "@/solve";
-import {stripIndents} from 'common-tags'
+import { stripIndents } from "common-tags";
 
 solve({
-    part1: {
-        tests: [
-            {
-                input: stripIndents``,
-                expected: 0
-            }
-        ],
-        fn: () => {
-            return 0;
-        }
+  part1: {
+    tests: [
+      {
+        input: stripIndents``,
+        expected: 0,
+      },
+    ],
+    fn: () => {
+      return 0;
     },
-    part2: {
-        tests: [
-            {
-                input: stripIndents``,
-                expected: 0
-            }
-        ],
-        fn: () => {
-            return 0;
-        }
-    }
+  },
+  part2: {
+    tests: [
+      {
+        input: stripIndents``,
+        expected: 0,
+      },
+    ],
+    fn: () => {
+      return 0;
+    },
+  },
 });
